2b9540e9480f90d9e6ad101cc5f63dfbc476eae3
Sneha <jayavardhini.sneha@gmail.com>
2023-08-21 18:10:28 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
 
 
 function App() {
+  const codingExerciseTitle = "React Coding Exercise"
+  const attributeList = ATTRIBUTE_LIST.reduce((acc, attribute) => {
+    acc[attribute] = 10;
+    return acc;
+  }, {});
+
   const [num, setNum] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Coding Exercise</h1>
+      <h1>{codingExerciseTitle}</h1>
       </header>
       <section className="App-section">
         <div>
