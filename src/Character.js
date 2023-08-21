b33a0652c54cc44a0ceec8a10e722b8e40fa3aff
Sneha <jayavardhini.sneha@gmail.com>
2023-08-21 20:02:00 -0300

    Maximum atrribute points

--- src/Character.js
+++ src/Character.js
@@ -1,24 +1,34 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useCallback } from 'react';
 import './App.css';
 import {ClassList} from "./ClassList";
 import { CLASS_LIST, } from './consts.js';
 import { AttributeList } from './AttributeList';
 
+
 function Character({ character }) {
   const [attributeValues, setAttributeValues] = useState(character.attributes);
 
   const incrementAttribute = (attribute) => {
+    if (totalModifierValue() < 70) {
       setAttributeValues((prevValues) => ({
         ...prevValues,
         [attribute]: prevValues[attribute] + 1,
       }));
+    }
   };
 
-  const decrementAttribute = (attribute) => {
+  const decrementAttribute = useCallback((attribute) => {
     setAttributeValues((prevValues) => ({
       ...prevValues,
       [attribute]: prevValues[attribute] - 1,
     }));
+  },[]);
+
+
+  const totalModifierValue = () => {
+    const maximum = Object.values(attributeValues).reduce((acc, ele) => acc + ele)
+    if (maximum === 70) { window.alert(`${character.name} can have upto 70 attribute points`) }
+    return maximum;
   };
 
   const doesCharacterMeetRequirements = useMemo(() => {
